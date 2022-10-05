4b8f2138089eb1fd15ac595de5335585a88f992f
Candela Brassesco <candebrassesco@gmail.com>
2022-10-05 18:30:37 -0300

    Agrego jQuery y lo aplico al div de resultado

--- js/index.js
+++ js/index.js
@@ -19,11 +19,10 @@ class Pestañas {
 const pestaña1 = new Pestañas ("Calculá tus cuotas", 1, "#calculadora")
 const pestaña2 = new Pestañas ("Qué es calQ", 2, "#infoCalQ")
 const pestaña3 = new Pestañas ("Por qué calQ", 3, "#motivosCalQ")
-const pestaña4 = new Pestañas ("Testimonios", 4, "#testimonio")
 
 // Creo el array correspondiente a las pestañas
 
-const listaPestañas = [pestaña1, pestaña2, pestaña3, pestaña4]
+const listaPestañas = [pestaña1, pestaña2, pestaña3]
 
 // Creo cada li del navbar empleando un forEach sobre el array, junto con un getElementById y un createElement
 
@@ -44,14 +43,28 @@ ulPestañas.appendChild(nuevaPestaña)
 // Hago un evento para cada vez que el usuario seleccione las cuotas e ingrese el monto,
 // aparezca un cuadro de texto que indique como le quedan las cuotas.
 
+// Función para redondear a dos decimales la función de 
+
+function roundToTwo(num) {
+    return +(Math.round(num + "e+2")  + "e-2");
+}
+
+
 // Defino la función que va a calcular las cuotas.
 
 function calculadoraInteres (monto, cuotas, interes){
     let montoTotal = monto+(monto*interes)
     let montoCuotas = montoTotal/cuotas 
-    alert(`El monto a pagar es de $${montoTotal} dividido en ${cuotas} cuota/s de $${montoCuotas}`)                   
+    let montoFinal = document.getElementById("montoFinal")
+    let cuotasPagar = document.getElementById ("cantidadDeCuotas")
+    let pagoCuota = document.getElementById("pagoPorCuota")
+    montoFinal.value = montoTotal
+    pagoCuota.value = roundToTwo(montoCuotas)
+    cuotasPagar.value = cuotas
+    // console.log(`El monto a pagar es de $${montoTotal} dividido en ${cuotas} cuota/s de $${montoCuotas}`)                   
  }
 
+// oculto el div con el resultado de las cuotas:
 
 
 // capturo el input de monto:
@@ -66,6 +79,9 @@ let interesSumado = document.getElementById("disabledTextInput-interes")
 
 let calculador = document.getElementById("botonCalculador")
 
+// Empleando funciones de la librería de jQuery oculto la sección de resultados.
+$("#resultado").hide();
+
 // Para poder acceder al valor de las cuotas:
 
 let select = document.getElementById("inputGroupSelect01-cuotas");
@@ -73,17 +89,17 @@ select.addEventListener('change',()=> {
     let cuotaSeleccionada = select.options[select.selectedIndex];
     if (parseInt(cuotaSeleccionada.text) == 12) {
         interesSumado.value = 0.15;
-        console.log(parseInt(cuotaSeleccionada.text))
+        console.log(parseInt(cuotaSeleccionada.value))
         console.log(interesSumado.value)  
-    } else if (parseInt(cuotaSeleccionada.text) == 18){
+    } else if (parseInt(cuotaSeleccionada.value) == 18){
         interesSumado.value = 0.18; 
-        console.log(parseInt(cuotaSeleccionada.text))
+        console.log(parseInt(cuotaSeleccionada.value))
         console.log(interesSumado.value)  
     } else {
         interesSumado.value = 0;
         console.log(interesSumado.value)  
     } 
-    localStorage.setItem("opcionCuota", cuotaSeleccionada.text)
+    localStorage.setItem("opcionCuota", cuotaSeleccionada.value)
   });
 
 // Evento para calcular las cuotas al apretar el botón "calcular":
@@ -91,6 +107,8 @@ select.addEventListener('change',()=> {
 calculador.addEventListener("click",()=>{
     let cuotas = parseInt(localStorage.getItem("opcionCuota"))
     calculadoraInteres(parseInt(montoAPagar.value), cuotas, interesSumado.value)
+    // Con la función show() de jQuery muestro la sección que había quedado oculta.
+    $("#resultado").show();
 }) 
 
 
